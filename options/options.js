ebe48e2b17ab2f529cd58a7d135d2eee015e8a0d
Crowdbotics <team@crowdbotics.com>
2023-07-30 16:06:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-hall-42525",
+  url: "https://fragrant-hall-42525.botics.co",
+  api: "https://fragrant-hall-42525.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
